113c3148fb3389bfecd777649c4cd9d052614ea0
tessleo99 <t.napoletano9@gmail.com>
2023-03-30 16:26:05 +0200

    scritte quadrattini e stile personalizzato

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 //GENERO NUMERI DA 1 A 100
 for (let i = 1; i<=100; i++){ 
     
-    //CONTROLLO SE I E' MULTIPLO SIA DI 5 CHE 3 E STAMPO FIZZBUZZ
+    //CONTROLLO SE i E' MULTIPLO SIA DI 5 CHE 3 E STAMPO FIZZBUZZ
     if (i%3===0 && i%5===0){    
         document.getElementById("container-for-squares").innerHTML += `
         <div class="square-fizz-buzz">
-            <p>${i}</p>
+            <p>FIZZBUZZ</p>
         </div>
         `; 
         console.log("FizzBuzz");
-    //CONTROLLO SE I E' MULTIPLO DI 3 E STAMPO FIZZ   
+    //CONTROLLO SE i E' MULTIPLO DI 3 E STAMPO FIZZ   
     } else if (i%3===0){
         document.getElementById("container-for-squares").innerHTML += `
         <div class="square-fizz">
-            <p>${i}</p>
+            <p>FIZZ</p>
         </div>
         `;
         console.log("Fizz");
-    //CONTROLLO SE I E' MULTIPLO DI 5 E STAMPO BUZZ
+    //CONTROLLO SE i E' MULTIPLO DI 5 E STAMPO BUZZ
     } else if (i%5===0){
         document.getElementById("container-for-squares").innerHTML += `
         <div class="square-buzz">
-            <p>${i}</p>
+            <p>BUZZ</p>
         </div>
         `;
         console.log("Buzz");
-    //SE I NON RISPETTA LE CONDIZIONI ALLORA LO FACCIO STAMPARE 
+    //SE i NON RISPETTA LE CONDIZIONI ALLORA LO FACCIO STAMPARE 
     } else {
          document.getElementById("container-for-squares").innerHTML += `
         <div class="square">
