2993d2bbde7fcf1285a37e51e6a452f6960ccfcd
tessleo99 <t.napoletano9@gmail.com>
2023-03-30 15:56:19 +0200

    creazione quadrattini

--- js/main.js
+++ js/main.js
@@ -2,16 +2,38 @@
 for (let i = 1; i<=100; i++){ 
     
     //CONTROLLO SE I E' MULTIPLO SIA DI 5 CHE 3 E STAMPO FIZZBUZZ
-    if (i%3===0 && i%5===0){     
+    if (i%3===0 && i%5===0){    
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square-fizz-buzz">
+            <p>${i}</p>
+        </div>
+        `; 
         console.log("FizzBuzz");
     //CONTROLLO SE I E' MULTIPLO DI 3 E STAMPO FIZZ   
     } else if (i%3===0){
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square-fizz">
+            <p>${i}</p>
+        </div>
+        `;
         console.log("Fizz");
     //CONTROLLO SE I E' MULTIPLO DI 5 E STAMPO BUZZ
     } else if (i%5===0){
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square-buzz">
+            <p>${i}</p>
+        </div>
+        `;
         console.log("Buzz");
     //SE I NON RISPETTA LE CONDIZIONI ALLORA LO FACCIO STAMPARE 
     } else {
+         document.getElementById("container-for-squares").innerHTML += `
+        <div class="square">
+            <p>${i}</p>
+        </div>
+        `;
          console.log(i);
     }
 }
+
+
